d589833807b1efb66bf836284ad1773be6117995
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-19 15:24:20 -0700

    added content to bio main
    will allow users to see a few details about myself

--- scripts/slider.js
+++ scripts/slider.js
@@ -22,9 +22,9 @@ function previousSlide() {
     var currentSlide = slides.map(ind => ind).filter(ind => $(slides[ind]).css('display') === 'block');
     var nextSlide = currentSlide[0] === 2 ? 1 : (currentSlide[0] === 1 ? 0 : 2);
     console.log('current: ', nextSlide)
-    $(slides[currentSlide[0]]).fadeOut(400);
+    $(slides[currentSlide[0]]).fadeOut(350);
     $(discs[currentSlide[0]]).css('background', 'rgba(255, 255, 255, 0.5');
-    $(slides[nextSlide]).delay(400).fadeIn(400);
+    $(slides[nextSlide]).delay(400).fadeIn(350);
     $(discs[nextSlide]).css('background', 'rgba(255, 255, 255, 1');
 
 }
