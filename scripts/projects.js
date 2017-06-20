8f9304d19d77ecabd6514e8ad3e7e14faa211b5b
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-06-20 13:48:24 -0700

    Removed indentation and linter errors.

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,41 +4,40 @@ var allRepos = [];
 var myRepos;
 
 function Repo(repoObject) {
-    Object.keys(repoObject).forEach(key => this[key] = repoObject[key])
-    allRepos.push(this);
+  Object.keys(repoObject).forEach(key => this[key] = repoObject[key]);
+  allRepos.push(this);
 
 }
 
 Repo.prototype.render = function() {
-    var template = Handlebars.compile($('#project-template').text())
-    return template(this);
+  var template = Handlebars.compile($('#project-template').text());
+  return template(this);
 };
 
-
 function fetchJSON() {
-        $.getJSON('https://api.github.com/users/ejdelrio/repos?callback=?').then(function(data) {
-                myRepos = dataCheck(data)
-                pushData(myRepos)
-                localStorage.myRepos = JSON.stringify(myRepos);
-            },
-            function(err) {
-                console.error(err)
-                myRepos = JSON.parse(localStorage.myRepos);
-                pushData(myRepos);
-            });
+  $.getJSON('https://api.github.com/users/ejdelrio/repos?callback=?').then(function(data) {
+    myRepos = dataCheck(data);
+    pushData(myRepos);
+    localStorage.myRepos = JSON.stringify(myRepos);
+  },
+  function(err) {
+    console.error(err);
+    myRepos = JSON.parse(localStorage.myRepos);
+    pushData(myRepos);
+  });
 }
 
 function pushData(source) {
-    source.data.forEach(ind => new Repo(ind));
-    allRepos.forEach(ind => $($('#projects').children('section').append(ind.render())));
+  source.data.forEach(ind => new Repo(ind));
+  allRepos.forEach(ind => $($('#projects').children('section').append(ind.render())));
 }
 
 function dataCheck(data) {
-    if(localStorage.myRepos) {
-        return JSON.parse(localStorage.myRepos).meta.ETag === data.meta.ETag ? JSON.parse(localStorage.myRepos) : data;
-    } else {
-        return data
-    }
+  if(localStorage.myRepos) {
+    return JSON.parse(localStorage.myRepos).meta.ETag === data.meta.ETag ? JSON.parse(localStorage.myRepos) : data;
+  } else {
+    return data;
+  }
 }
 
 fetchJSON();
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -4,14 +4,14 @@ var navTabs = $('nav').find('li');
 
 
 function navHandler() {
-    navTabs.each((ind, el) => $(el).on('click', function () {
-        var targetMain = $($(el).children('p')).text();
+  navTabs.each((ind, el) => $(el).on('click', function () {
+    var targetMain = $($(el).children('p')).text();
 
-        $('#' + targetMain.toLowerCase()).show();
-        $(navTabs.find('p')).filter((ind, tag) => $(tag).text() !== targetMain).each((ind, tag) =>
-            $(`#${$(tag).text().toLowerCase()}`).hide())
+    $('#' + targetMain.toLowerCase()).show();
+    $(navTabs.find('p')).filter((ind, tag) => $(tag).text() !== targetMain).each((ind, tag) =>
+      $(`#${$(tag).text().toLowerCase()}`).hide());
 
-    }));
+  }));
 
 }
 
